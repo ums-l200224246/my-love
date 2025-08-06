814d8fa5bf49a3fa11dfc2b41091bc29510a8d57
Rendra Official <rendraofc@gmail.com>
2025-08-06 11:07:25 +0000

    kembalikan ke versi sebelum aku minta agar bisa di hosting github pages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/my-birthday-app',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
   },
 };
 
